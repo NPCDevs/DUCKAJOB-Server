04121487370282cceb2f046e59267c72ec0deef7
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-03-17 20:20:30 +0000

    Bot can now speak

--- server.js
+++ server.js
@@ -17,18 +17,60 @@ connectDB();
 
 const token = process.env.BOT_TOKEN;
 
-// const bot = new TelegramApi(token, { polling: true });
+const bot = new TelegramApi(token, { polling: true });
 
-// bot.on('message', (msg) => {
-//   const text = msg.text;
-//   const chatId = msg.chat.id;
-//   switch (text) {
-//     case '/help':
-//       bot.sendMessage(chatId, `Text for help command`);
-//       break;
-//     default:
-//       break;
-//   }
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3) + 1;
+}
+
+bot.on('message', async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  switch (text) {
+    case '/start':
+      await bot.sendSticker(
+        chatId,
+        'CAACAgIAAxkBAANGZBTHhyAI3v2himIPQqTTC4i5fZMAAgEBAAJWnb0KIr6fDrjC5jQvBA',
+      );
+      bot.sendMessage(
+        chatId,
+        `Welcome to Duck a Job, the ultimate freelancing app that leverages the power of smart contracts on the TON blockchain to facilitate secure and transparent transactions between employers and freelancers. You can start using the bot by pressing button "Open app"`,
+      );
+      break;
+    case '/help':
+      bot.sendMessage(chatId, `Text for help command`);
+      break;
+    default:
+      const randomNumber = getRandomNumber();
+      switch (randomNumber) {
+        case 1:
+          bot.sendSticker(
+            chatId,
+            'CAACAgIAAxkBAANrZBTKGK32Pt_Z55C0fcsPSXQl2iUAAvkAA1advQqVZW6rKisbNi8E',
+          );
+          break;
+
+        case 2:
+          bot.sendSticker(
+            chatId,
+            'CAACAgIAAxkBAAN_ZBTLEBOkBbxjIXDpk3h1A3g_BQoAAgsBAAJWnb0KTrHnpgj77UkvBA',
+          );
+          break;
+
+        default:
+          bot.sendSticker(
+            chatId,
+            'CAACAgIAAxkBAANvZBTKVZDmZn0XnOKD0fHeOdrpApsAAgIBAAJWnb0KTuJsgctA5P8vBA',
+          );
+          break;
+      }
+
+      break;
+  }
+});
+
+// bot.on('sticker', (msg) => {
+//   console.log(msg.sticker);
 // });
 
 server.use('/', require('./Routes/index'));
